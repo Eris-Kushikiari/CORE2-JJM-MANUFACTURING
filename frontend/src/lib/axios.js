5b1c5a308469b2e0df41a279d383c47205027f1e
Eris-Kushikiari <erishallypena@gmail.com>
2025-09-05 14:47:48 +0800

    Updated the axios.js. Ready to Deploy

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? `${import.meta.env.VITE_SOCKET_SERVER}`
-      : "https://backend-core2.jjm-manufacturing.com/api",
+  baseURL: import.meta.env.VITE_SOCKET_SERVER,
   withCredentials: true,
 });
 
