c8c6654a04e95f3de144582a2c6fd8c6bff1fd58
Eris-Kushikiari <erishallypena@gmail.com>
2025-09-05 18:25:31 +0800

    update in axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? `${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}`
-      : "https://core2-jjm-manufacturing.onrender.com",
+      : "https://core2-jjm-manufacturing.onrender.com/api",
   withCredentials: true,
 });
 
