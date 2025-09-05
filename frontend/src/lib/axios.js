6d1d405563c269383ba7aa2394975064b37f535b
Eris-Kushikiari <erishallypena@gmail.com>
2025-09-05 13:48:41 +0800

    Deploying to vercel

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "http://localhost:7684/api"
+      ? `${import.meta.env.VITE_SOCKET_SERVER}`
       : "https://backend-core2.jjm-manufacturing.com/api",
   withCredentials: true,
 });
