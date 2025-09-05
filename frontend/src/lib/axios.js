267667b99d8b53d34ad7f4712eaa0e28a87c08a3
Eris-Kushikiari <erishallypena@gmail.com>
2025-09-05 18:19:40 +0800

    Fixed axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? `${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}`
-      : "https://core-2-jjm-manufacturing.vercel.app/api",
+      : "https://core2-jjm-manufacturing.onrender.com",
   withCredentials: true,
 });
 
